4fed2961c85bac9575df29c0f403d42b0b5da50b
assadanabosi <assad.anabosi@icloud.com>
2022-07-16 22:56:43 +0300

    loading animation

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,6 @@ export const UserContext = React.createContext();
 
 function App() {
 	const u = useSelector((state) => state.user.user);
-	// console.log(u);
 	const [isOpen, setIsOpen] = useState(false);
 	const [user, setUser] = useState(null);
 	const dispatch = useDispatch();
@@ -80,37 +79,36 @@ function App() {
 				<Routes>
 					<Route path="/" exact element={<Home />} />
 					<Route path="/login" exact element={!user ? <Login /> : <Navigate to="/" />} />
-					{/* <Route element={<AuthRoutes />}> */}
-					<Route path="/changePassword" exact element={<ChangePassword />} />
-					<Route path="/logs" exact element={<Logs />} />
-					{/* <Route path="/myLogs" exact element={<Test />} /> */}
-					{/* <Route element={<AdminRoutes />}> */}
-					{/* Bills Routes */}
-					<Route path="/bills" exact element={<Bills />} />
-					<Route path="/bills/new" exact element={<NewBill />} />
-					<Route path="/bills/:id" exact element={<Bill />} />
-					<Route path="/bills/:id/edit" exact element={<Test />} />
-					{/* Workers Routes */}
-					<Route path="/workers" exact element={<Workers />} />
-					<Route path="/workers/new" exact element={<WorkerForm />} />
-					<Route path="/workers/:id" exact element={<Worker />} />
-					<Route path="/workers/:id/edit" exact element={<WorkerForm />} />
-					{/* Logs Routes */}
-					<Route path="/logs/new" exact element={<Test />} />
-					<Route path="/logs/:id" exact element={<Log />} />
-					<Route path="/logs/:id/edit" exact element={<Test />} />
-					{/* Payees Routes */}
-					<Route path="/payees" exact element={<Payees />} />
-					<Route path="/payees/new" exact element={<PayeeForm />} />
-					<Route path="/payees/:id" exact element={<Payee />} />
-					<Route path="/payees/:id/edit" exact element={<PayeeForm />} />
-					{/* Cheques Routes */}
-					<Route path="/cheques" exact element={<Cheques />} />
-					<Route path="/cheques/new" exact element={<Test />} />
-					<Route path="/cheques/:id" exact element={<Cheque />} />
-					<Route path="/cheques/:id/edit" exact element={<Test />} />
-					{/* </Route> */}
-					{/* </Route> */}
+					<Route element={<AuthRoutes />}>
+						<Route path="/changePassword" exact element={<ChangePassword />} />
+						<Route path="/logs" exact element={<Logs />} />
+						<Route element={<AdminRoutes />}>
+							{/* Bills Routes */}
+							<Route path="/bills" exact element={<Bills />} />
+							<Route path="/bills/new" exact element={<NewBill />} />
+							<Route path="/bills/:id" exact element={<Bill />} />
+							<Route path="/bills/:id/edit" exact element={<Test />} />
+							{/* Workers Routes */}
+							<Route path="/workers" exact element={<Workers />} />
+							<Route path="/workers/new" exact element={<WorkerForm />} />
+							<Route path="/workers/:id" exact element={<Worker />} />
+							<Route path="/workers/:id/edit" exact element={<WorkerForm />} />
+							{/* Logs Routes */}
+							<Route path="/logs/new" exact element={<Test />} />
+							<Route path="/logs/:id" exact element={<Log />} />
+							<Route path="/logs/:id/edit" exact element={<Test />} />
+							{/* Payees Routes */}
+							<Route path="/payees" exact element={<Payees />} />
+							<Route path="/payees/new" exact element={<PayeeForm />} />
+							<Route path="/payees/:id" exact element={<Payee />} />
+							<Route path="/payees/:id/edit" exact element={<PayeeForm />} />
+							{/* Cheques Routes */}
+							<Route path="/cheques" exact element={<Cheques />} />
+							<Route path="/cheques/new" exact element={<Test />} />
+							<Route path="/cheques/:id" exact element={<Cheque />} />
+							<Route path="/cheques/:id/edit" exact element={<Test />} />
+						</Route>
+					</Route>
 					{/* Errors */}
 					<Route path="/500" exact element={<Error />} />
 					<Route path="*" exact element={<NotFound />} />
