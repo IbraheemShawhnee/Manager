7d0a39567c7ebc7254d68aa9bfc1eaec0ecfc3d1
Assad Anabosi <assad.anabosi@icloud.com>
2022-07-29 17:26:49 +0300

    fix: bug at app.js

--- app.js
+++ app.js
@@ -1,48 +1,46 @@
+import dotenv from "dotenv"
 if (process.env.NODE_ENV !== "production") {
-	require('dotenv').config();
+	dotenv.config();
 }
-const PORT = process.env.PORT || 5000;
-const FRONTEND_PORT = process.env.FRONTEND_PORT || 3000;
-const CLIENT = `http://localhost:${FRONTEND_PORT}`;
+const PORT = process.env.PORT || 80;
 const MONGOD_PORT = process.env.DB_PORT || 27017;
 
 //	PACKAGES
-const express = require("express");
-const path = require("path");
-const ejsMate = require("ejs-mate");
-const methodOverride = require("method-override");
-const mongoose = require("mongoose");
-//	Session
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
+import express from "express"
+import path from "path"
+import ejsMate from "ejs-mate"
+import methodOverride from "method-override"
+import mongoose from "mongoose"
+import session from "express-session"
+import flash from "connect-flash"
+import passport from "passport"
+import LocalStrategy from "passport-local"
+import mongoSanitize from "express-mongo-sanitize"
+import helmet from "helmet"
+import MongoStore from "connect-mongo"
+import User from "./models/user.js"
 
-const flash = require("connect-flash")
+//	Path
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const passport = require("passport");
-const LocalStrategy = require("passport-local")
-const mongoSanitize = require("express-mongo-sanitize");
-
-const helmet = require("helmet");
-const cors = require("cors");
-const User = require("./models/user");
-
-// Routes and Authorizations
-const { isLoggedIn, isAdmin } = require("./middlewares/middleware");
-const APIRoutes = require("./API/api")
-const billsRoutes = require("./routes/bills")
-const workersRoutes = require("./routes/workers")
-const logsRoutes = require("./routes/logs")
-const payeesRoutes = require("./routes/payees")
-const chequesRoutes = require("./routes/cheques")
-const usersRoutes = require("./routes/users")
+// Routes
+import { isLoggedIn, isAdmin } from "./middlewares/middleware.js"
+import billsRoutes from "./routes/bills.js"
+import workersRoutes from "./routes/workers.js"
+import logsRoutes from "./routes/logs.js"
+import payeesRoutes from "./routes/payees.js"
+import chequesRoutes from "./routes/cheques.js"
+import usersRoutes from "./routes/users.js"
 
 // Error handling
-const ExpressError = require("./utils/ExpressError");
-const catchAsync = require("./utils/catchAsync");
+import ExpressError from "./utils/ExpressError.js"
+import catchAsync from "./utils/catchAsync.js"
 
 //	DB CONNECTION
 
-dbUrl = process.env.DB_URL || "mongodb://localhost:" + MONGOD_PORT + "/managerDB"
+const dbUrl = process.env.DB_URL || "mongodb://localhost:" + MONGOD_PORT + "/managerDB"
 mongoose.connect(dbUrl, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
@@ -50,9 +48,9 @@ mongoose.connect(dbUrl, {
 	.then(() => {
 		console.log("Connection to the Database was established successfully!")
 	})
-	.catch(error => {
+	.catch(err => {
 		console.log("An ERROR occurred while attempting to connect to the Database")
-		console.log(error)
+		console.log(err)
 	})
 mongoose.connection.once("open", async () => {
 	if (await User.countDocuments().exec() < 1) {
@@ -103,41 +101,18 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-//	Middlewares
-
-//	Parsing JSON
 app.use(express.urlencoded({ extended: true }));
-//	Serving files
 app.use(express.static(path.join(__dirname, "/public")));
-//	Method Overriding
 app.use(methodOverride("_method"));
-//	Configuring Session
 app.use(session(sessionConfig));
-//	Flash
 app.use(flash());
-//	Helmet
 app.use(helmet());
-//	Mongo Sanitize
 app.use(mongoSanitize({
 	replaceWith: '_'
 }));
-//	--
-app.use(express.json());
-app.use(cors({
-	origin: CLIENT,
-	methods: "GET,POST,PUT,PATCH,DELETE",
-	credentials: true,
-}));
 
-//	Passport session manager
 app.use(passport.initialize());
 app.use(passport.session());
-//	Passport Configuration
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// Configuring Helemt's Content Security Policy
 
 const scriptSrcUrls = [
 	"https://stackpath.bootstrapcdn.com",
@@ -145,7 +120,7 @@ const scriptSrcUrls = [
 ];
 const styleSrcUrls = [
 	"https://stackpath.bootstrapcdn.com",
-	"https://cdn.jsdelivr.net/",
+	"https://cdn.jsdelivr.net/"
 ];
 const connectSrcUrls = [
 
@@ -167,8 +142,6 @@ app.use(
 				"'self'",
 				"blob:",
 				"data:",
-				"https://www.transparenttextures.com",
-				"https://www.transparenttextures.com/patterns/carbon-fibre-big.png",
 				"https://images.unsplash.com",
 			],
 			fontSrc: ["'self'", ...fontSrcUrls],
@@ -176,18 +149,19 @@ app.use(
 	})
 );
 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //	Main Middleware
 app.use((req, res, next) => {
 	res.locals.currentUser = req.user;
-	//	Flasing
 	res.locals.success = req.flash("success");
 	res.locals.error = req.flash("error");
 	next();
 });
 
-//	Routes and Authorizations
-app.use("/api", APIRoutes)
+//	Routes and their Authorizations
 app.use("/bills", isLoggedIn, isAdmin, billsRoutes);
 app.use("/payees", isLoggedIn, isAdmin, payeesRoutes);
 app.use("/workers", isLoggedIn, isAdmin, workersRoutes);
@@ -201,17 +175,15 @@ app.get("/", (req, res) => {
 })
 
 app.all('*', (req, res, next) => {
-	console.log(`(404)Request at: ${req.originalUrl}`);
 	next(new ExpressError("Page  Not Found", 404));
 })
-
 
 app.use((err, req, res, next) => {
 	const { statusCode = 500 } = err;
 	if (!err.message) err.message = "Oh No, Something Went Wrong!"
 	return res.status(statusCode).render("error", { pageTitle: "Error", err: err })
 })
-// Start server
+
 app.listen(PORT, () => {
-	console.log(`Server has started on PORT: ${PORT}`);
+	console.log("Server has started on PORT:" + PORT);
 })
