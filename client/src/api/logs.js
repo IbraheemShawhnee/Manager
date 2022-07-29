8315b5313ee529540d5a025f2e5f77a4b37d6085
Assad Anabosi <assad.anabosi@icloud.com>
2022-07-29 16:44:27 +0300

    delete api folder

--- client/src/api/logs.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const fetchLogs = () => axios.get("/api/logs");
-export const findLog = (id) => axios.get(`/api/logs/${id}`);
-export const fetchMyLogs = () => axios.get("/api/logs/myLogs");
-export const createLog = (data) => axios.post("/api/logs", data);
-export const updateLog = (data, id) => axios.put(`/api/logs/${id}`, data);
--- client/src/api/workers.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const fetchWorkers = () => axios.get("/api/workers");
-export const findWorker = (id) => axios.get(`/api/workers/${id}`);
-export const createWorker = (data) => axios.post("/api/register", data);
-export const updateWorker = (data, id) => axios.put(`/api/workers/${id}`, data);
