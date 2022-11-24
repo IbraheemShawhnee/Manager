8c7ddca999863bf818886481b7de3fea8d0c0ac2
Assad Anabosi <assad.anabosi@icloud.com>
2022-11-24 20:35:16 +0200

    code clean

--- API/middlewares/middleware.js
+++ API/middlewares/middleware.js
@@ -1,7 +1,4 @@
 import User from "../../models/user.js";
-// import Bill from "../../models/bill.js";
-// import Cheque from "../../models/cheque.js";
-// import Log from "../../models/log.js";
 
 export const isLoggedIn = (req, res, next) => {
 	if (!req.isAuthenticated()) {
@@ -36,41 +33,3 @@ export const isSuper = async (req, res, next) => {
 	}
 	next();
 }
-
-// export const isSuperV = async (req) => {
-// 	const user = await User.findById(req.user._id);
-// 	return user.isSuper;
-// }
-
-// export const chequesPaginatedResult = async (page, startIndex, endInex) => {
-// 	let pages = {}
-// 	if (endInex < await Cheque.countDocuments().exec()) {
-// 		pages.next = page + 1;
-// 	}
-// 	if (startIndex > 0) {
-// 		pages.pervious = page - 1;
-// 	}
-// 	return pages;
-// }
-
-// export const logsPaginatedResult = async (page, startIndex, endInex) => {
-// 	let pages = {}
-// 	if (endInex < await Log.countDocuments().exec()) {
-// 		pages.next = page + 1;
-// 	}
-// 	if (startIndex > 0) {
-// 		pages.pervious = page - 1;
-// 	}
-// 	return pages;
-// }
-
-// export const billsPaginatedResult = async (page, startIndex, endInex) => {
-// 	let pages = {}
-// 	if (endInex < await Bill.countDocuments().exec()) {
-// 		pages.next = page + 1;
-// 	}
-// 	if (startIndex > 0) {
-// 		pages.pervious = page - 1;
-// 	}
-// 	return pages;
-// }
